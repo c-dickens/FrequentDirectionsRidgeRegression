51b341003d4f758650a39801d1b5dc7f469c43df
c-dickens <c.dickens@warwick.ac.uk>
2020-11-10 10:51:53 +0000

    Updated docstring

--- src/frequent_directions.py
+++ src/frequent_directions.py
@@ -10,20 +10,18 @@ class FrequentDirections:
         subsequent subclasses which inherit from FrequentDirections.
         '''
         self.d = d
-        #self.reset(sketch_dim=m)
         self.delta = 0. # For RFD
 
         if sketch_dim is not None:
             self.sketch_dim = sketch_dim
         self.sketch = np.zeros((self.sketch_dim,self.d),dtype=float)
         self.Vt = np.zeros((self.sketch_dim,self.d),dtype=float)
-        self.sigma_squared = np.zeros(self.sketch_dim,dtype=float) # singular values squared
+        self.sigma_squared = np.zeros(self.sketch_dim,dtype=float) 
         
     def fit(self,X,batch_size=1):
         '''
         Fits the FD transform to dataset X
         '''
-        #self.sketch = np.concatenate()
         n = X.shape[0]
         for i in range(0,n,batch_size):
             batch = X[i:i+batch_size,:]
@@ -33,7 +31,7 @@ class FrequentDirections:
             except np.linalg.LinAlgError:
                 _, s, self.Vt = linalg.svd(aux, full_matrices=False, lapack_driver='gesvd')
             self.sigma_squared = s**2 
-            self.__rotate_and_reduce__()  # shrink self.s2 and self.Vt
+            self.__rotate_and_reduce__() 
             self.sketch = self.Vt * np.sqrt(self.sigma_squared).reshape(-1, 1)
         
     def get(self):
@@ -51,9 +49,10 @@ class FastFrequentDirections(FrequentDirections):
         
 class RobustFrequentDirections(FrequentDirections):
     '''
-    Implements the RFD version of FD 
+    Implements the RFD version of FD by maintaining counter self.delta.
+    Still operates in the `fast` regimen by doubling space, as in 
+    FastFrequentDirections
     '''
-    
     def __rotate_and_reduce__(self):
         self.delta += self.sigma_squared[self.sketch_dim]/2.
         self.sigma_squared = self.sigma_squared[:self.sketch_dim] - self.sigma_squared[self.sketch_dim]
